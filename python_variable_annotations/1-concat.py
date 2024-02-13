a3d3b69a0517d10bc4080f606c2c3fe57ee09be0
SacariLuz <asacaricachi@gmail.com>
2024-02-12 19:20:34 -0800

    a function contar that takes a string

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+A function concat
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+    returns
+    concatenation of two strings.
+    """
+    return "{}{}".format(str1, str2)
