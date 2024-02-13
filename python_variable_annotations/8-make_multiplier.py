6ba1101a761efc2f0a49c40e550a4dc737eb98de
SacariLuz <asacaricachi@gmail.com>
2024-02-12 21:27:49 -0800

    Write a type-annotated function make_multiplier that takes a float multiplier

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -6,8 +6,12 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """ return a callable """
+    """
+    return a callable
+    """
     def multiply(m: float) -> float:
-        """return m * multiplier :)"""
+        """
+        return m * multiplier :)
+        """
         return m * multiplier
     return multiply
