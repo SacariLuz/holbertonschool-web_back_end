525e52e75d4f236ecd4034fd55e26cbedbad7f1d
SacariLuz <asacaricachi@gmail.com>
2024-02-12 20:19:35 -0800

    a function sum_list which takes a list input_list

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+List of floats annotations
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Function sum_listtake list as argument.
+    input_list : list float
+    return : sum list
+    """
+
+    return sum(input_list)
