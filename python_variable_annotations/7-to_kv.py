7929a1690847124c3524cd065c8ccdc3c15a157c
SacariLuz <asacaricachi@gmail.com>
2024-02-12 21:28:59 -0800

    delete blank line

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -6,7 +6,6 @@ string and int/float to tuple.
 from typing import Union, Tuple
 
 
-
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     takes a string k and an int OR float v as arguments
