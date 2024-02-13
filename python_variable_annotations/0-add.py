2e03cf4e2b44b4474679a7549ff8f24781d9c072
SacariLuz <asacaricachi@gmail.com>
2024-02-12 19:03:44 -0800

    returns their sum as a float

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Anotacion add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Args: 
+    a' first parameter
+    b' second parameter
+
+    Return:
+    Sum two floats
+    """
+    return (a + b)
