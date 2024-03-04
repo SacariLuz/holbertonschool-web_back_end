9fa703a011ef577aa1ea56ec842457e2657eed72
SacariLuz <asacaricachi@gmail.com>
2024-03-04 02:36:52 +0000

    correction file

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 """
 that changes all topics of a school document
 """
@@ -10,8 +9,7 @@ def update_topics(mongo_collection, name, topics):
     Update topics in a collection
     name (str): Name of the school whose topics to update
     """
-
-     mongo_collection.update_many(
-        {"name": name},
-        {"$set": {"topics": topics}}
-    )
+    mongo_collection.update_many(
+            {"name": name},
+            {"$set": {"topics": topics}}
+            )
