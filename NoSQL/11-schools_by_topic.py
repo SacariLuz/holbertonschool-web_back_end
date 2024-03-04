0f90ef34510412d281c024ea15b047d71db5679f
SacariLuz <asacaricachi@gmail.com>
2024-03-04 03:03:26 +0000

    correction indent

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -7,9 +7,7 @@ Returns the list of school having a specific topic
 def schools_by_topic(mongo_collection, topic):
     """
     Finds collection, based in the schools
-
     mongo_collection: pymongo collection object
-
     returns: the list of school having a specific topic
     """
      school_lists = mongo_collection.find({"topics": topic})
