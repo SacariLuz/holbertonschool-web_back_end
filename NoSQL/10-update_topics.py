baf413a07a74e8f61515950e65c322c4fafcd944
SacariLuz <asacaricachi@gmail.com>
2024-03-04 02:34:27 +0000

    change school topicts

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+that changes all topics of a school document
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Update topics in a collection
+    name (str): Name of the school whose topics to update
+    """
+
+     mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
