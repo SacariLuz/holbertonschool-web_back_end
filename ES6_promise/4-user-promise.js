1258fe6f0939fdc003bdbcf939aba6231d33219c
SacariLuz <asacaricachi@gmail.com>
2024-04-15 03:52:02 +0000

    correction sintsx

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,8 +1,10 @@
-export default function signUpUser(firstName, lastName) {
-  return new Promise((resolve) => {
-    resolve({
+function signUpUser(firstName, lastName) {
+  return Promise.resolve(
+    {
       firstName,
       lastName,
-	});
-  });
+    },
+  );
 }
+
+export default signUpUser;
