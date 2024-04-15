12cf6e17d91499fc1449f5e8d2454288cf44ae4c
SacariLuz <asacaricachi@gmail.com>
2024-04-15 03:41:58 +0000

    Handle multiple promises

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,15 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const user = await signUpUser(firstName, lastName).then((value) => ({
+	status: 'fulfilled',
+	value,
+  }));
+  const photography = await uploadPhoto(fileName).catch((error) => ({
+	status: 'rejected',
+	value: `${error.name}: ${error.message}`,
+  }));
+
+  return [user, photography];
+}
